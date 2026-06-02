a09985212617665d2f15f0f304d8b1d5c2d67e14
kavyaankem <ankemkavyaa03@gmail.com>
2026-06-02 15:30:55 +0530

    Java Script page

---
+++ Page/page.js
@@ -0,0 +1,9 @@
+function onClickToOpen() {
+    debugger;
+    alert("You clicked the button!");
+    console.log("Button was clicked!");
+    document.getElementById("name").value = "The button was clicked!";
+
+    
+}
+
